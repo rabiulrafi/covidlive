1a1590d32d5f2a6528f4201a88c419c5c4a6f21b
rabiulrafi <rabiulrafi01@gmail.com>
2020-04-29 23:31:08 +0600

    CSS included

--- CovidTracker/settings.py
+++ CovidTracker/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
 MEDIA_URL = '/images/'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
